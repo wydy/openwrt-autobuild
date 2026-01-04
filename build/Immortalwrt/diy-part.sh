de95c25b9f486cb203782e55ad9b70d6b80f9758
Wydy <1937986+wydy@users.noreply.github.com>
2026-01-04 13:48:46 +0800

    Revert "Add make fixup command for openwrt-smartdns package"
    This reverts commit 11d4daf52df1ad155285c81c0ce1076a2c8640fd.

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -5,8 +5,6 @@
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
 
-make package/feeds/dstheme/openwrt-smartdns/check FIXUP=1
-
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
