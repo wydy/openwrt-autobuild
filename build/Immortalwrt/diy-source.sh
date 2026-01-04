4c0c44486e485b5dd8294f33af9d8f57464cd3fd
Wydy <1937986+wydy@users.noreply.github.com>
2026-01-04 21:45:55 +0800

    Update diy-source.sh

--- build/Immortalwrt/diy-source.sh
+++
@@ -1,4 +0,0 @@
-make package/feeds/dstheme/openwrt-smartdns/download
-make package/feeds/dstheme/openwrt-smartdns/check FIXUP=1
-make package/feeds/dstheme/openwrt-smartdns/download
-make package/feeds/dstheme/openwrt-smartdns/check FIXUP=1
